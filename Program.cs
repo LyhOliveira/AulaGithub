e7abd667350f6b0acba45cf61f1c1bfee8118cc8
LyhOliveira <135745636+LyhOliveira@users.noreply.github.com>
2023-06-07 14:23:54 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AulaGithub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola! Este é o meu projeto");
+            Console.WriteLine("Ele é um teste para o Github");
+            Console.WriteLine("Nada de novo por aqui...");
+            Console.ReadKey();
+
+        }
+
+    }
+}
